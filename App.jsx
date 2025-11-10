093d9d072bb870ba860fe50de097d2bc514b5f5d
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-10 20:14:11 +0530

    Added all pages

--- App.jsx
+++ App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import User from "./Pages/User";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/user" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
